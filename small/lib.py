0de9982adc1a301ce8d01b4d5298451480edbdd1
Chris Davies <mcd@daviesinc.com>
2013-01-26 17:47:07 -0500

    * modified to add a separate dogpile cache region with a 60 day timeout for rides * added params support for requests rather than building the urls through concatenation

--- small/lib.py
+++ small/lib.py
@@ -10,35 +10,47 @@ region = make_region().configure(
         'host': 'localhost',
         'port': 6379,
         'db': 1,
-        'redis_expiration_time': 60*60*24,
+        'redis_expiration_time': 60*60*1,
         'distributed_lock':True
         }
+)
+
+"""
+   add a second region with a 60 day cache for the rides to cut down on
+   API hits for clubs. Rides generally don't disappear.
+"""
+dbmregion = make_region().configure(
+    'dogpile.cache.dbm',
+    expiration_time = 60*60*24*60,
+    arguments = {
+        "filename":"/var/www/sscp/small/data/cachefile.dbm"
+    }
 )
 
 """
 little heavy on the caching as this really hammers the API server
 """
 
-def get_strava(uri, key):
-    r = requests.get(BASE_API + uri)
+def get_strava(uri, args, key):
+    r = requests.get(BASE_API + uri, params=args)
     try:
         return r.json()[key]
     except:
         return None
 
-@region.cache_on_arguments(expiration_time=3600)
+@region.cache_on_arguments()
 def get_clubs(name):
-    return get_strava('/clubs?name=%s' % quote_plus(name), 'clubs')
+    return get_strava('/clubs', {'name':name}, 'clubs')
 
-@region.cache_on_arguments(expiration_time=3600)
+@region.cache_on_arguments()
 def get_members(id):
-    return get_strava('/clubs/%s/members' % id, 'members')
+    return get_strava('/clubs/%s/members' % id, None, 'members')
 
-@region.cache_on_arguments(expiration_time=3600)
+@region.cache_on_arguments()
 def get_rides(id, date_start, date_end):
-    return get_strava('/rides?athleteId=%s&startDate=%s&endDate=%s' % \
-        (id, date_start, date_end) , 'rides')
+    return get_strava('/rides', {'athleteID':id, 'startDate':date_start, \
+        'endDate':date_end} , 'rides')
 
-@region.cache_on_arguments(expiration_time=86400)
+@dbmregion.cache_on_arguments()
 def get_ride(id):
-    return get_strava('/rides/%s' % id, 'ride')
+    return get_strava('/rides/%s' % id, None, 'ride')
